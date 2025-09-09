11b9a85ba3fea159c0e99b85f50c73e0743663f2
davidkupferstin <k6780519@gmail.com>
2025-09-09 18:01:11 +0300

    Fix asset_route for es

--- services/asset_route/unique_id_generator.py
+++ services/asset_route/unique_id_generator.py
@@ -4,9 +4,9 @@ class UniqueIDGenerator:
     def __init__(self):
         pass
 
-    def generates_id_from_metadata(self, meta_record):
-        size = meta_record['metadata']['size']
-        last_modified = meta_record['metadata']['last_modified']
+    def generates_id_from_metadata(self, metadata):
+        size = metadata['size']
+        last_modified = metadata['last_modified']
 
         unique_id = str(size) + last_modified.replace(" ", "")
 
